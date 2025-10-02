689c0bb2269ad8cd4f7eba315b37d4cb713a759d
JpegWriter <mattpantling@yahoo.co.uk>
2025-10-02 15:23:02 +0200

    Fix layout and contrast - re-enable error overlay

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 export default defineConfig({
   plugins: [
     react(),
-    // Temporarily disabled runtime error overlay
-    // runtimeErrorOverlay(),
+    runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
